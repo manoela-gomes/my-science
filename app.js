bef5c881824954456218fbcb7eda2fbb24556622
Manoela <manoela@MacBook-Air-de-Manoela.local>
2022-10-02 16:13:56 -0300

    Allow cors

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 /* Criar a rota raiz */
 app.post("/dalle", GenerateImage.generateImage)
 
